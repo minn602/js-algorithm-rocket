ee0cd9294315eeeb5215a12aeae39a802fa41824
cindy <minn602@gmail.com>
2021-09-07 11:13:39 +0900

    0907 문제풀이 minn602

---
+++ 1. Study/0907 - 기본문제/minn602/2. 최댓값 구하기.js
@@ -0,0 +1,5 @@
+function solution(arr) {
+  return Math.max(...arr);
+}
+
+console.log(solution([5, 3, 7, 11, 2, 15, 17]));
---
+++ 1. Study/0907 - 기본문제/minn602/5. 연필갯수.js
@@ -0,0 +1,5 @@
+function solution(n) {
+  return Math.floor(n / 12) + 1;
+}
+
+solution(25);
