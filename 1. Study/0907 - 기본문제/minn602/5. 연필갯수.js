4067c151b3e821a355abead3b3b0aef9c21b61df
cindy <minn602@gmail.com>
2021-09-09 22:37:34 +0900

    0907 문제 오답 minn602

--- 1. Study/0907 - 기본문제/minn602/5. 연필갯수.js
+++ 1. Study/0907 - 기본문제/minn602/5. 연필갯수.js
@@ -3,3 +3,14 @@ function solution(n) {
 }
 
 solution(25);
+
+//24명인 경우를 생각해서 조건문추가하기
+//ceil 메소드 사용하면 +1 연산을 할 필요가 없다.
+
+function solution(n) {
+  if (n % 12 === 0) {
+    return Math.floor(n / 12);
+  } else {
+    return Math.ceil(n / 12);
+  }
+}
