758d98b94cc966e77faaab945ec3d989b0b09222
ianlee92 <ethian9227@gmail.com>
2021-10-12 00:47:04 +0900

    1012 answer

--- 2. Answer/1005 - 효율성(슬라이딩윈도우, 해쉬).js
+++ 2. Answer/1005 - 효율성(슬라이딩윈도우, 해쉬).js
@@ -1,92 +1,28 @@
-// 📚 5. 최대 매출
-function solution(k, arr) {
+// Hash
+// 임의의 크기를 가진 데이터(Key)를 고정된 크기의 데이터(Value)로 변화시켜 저장하는 것
+// 해시 값 자체를 index로 사용하기 때문에 평균 시간 복잡도가 O(1)로 매우 빠름
+
+function classPresident(vote) {
   let answer,
-    sum = 0;
-  for (let i = 0; i < k; i++) sum += arr[i];
-  answer = sum;
-  for (let i = k; i < arr.length; i++) {
-    sum += arr[i] - arr[i - k];
-    answer = Math.max(answer, sum);
-  }
-  return answer;
-}
+    max = 0;
+  let hash = new Map();
 
-let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
-console.log(solution(3, a));
-
-// 📚 6. 학급 회장(해쉬)
-function solution(s) {
-  let answer;
-  let sH = new Map();
-  for (let x of s) {
-    if (sH.has(x)) sH.set(x, sH.get(x) + 1);
-    else sH.set(x, 1);
+  for (let x of vote) {
+    if (hash.has(x)) hash.set(x, hash.get(x) + 1);
+    else hash.set(x, 1);
   }
-  let max = Number.MIN_SAFE_INTEGER;
-  for (let [key, val] of sH) {
-    if (val > max) {
-      max = val;
+
+  console.log(hash);
+
+  for (let [key, value] of hash) {
+    if (value > max) {
+      max = value;
       answer = key;
     }
   }
+
   return answer;
 }
 
-let str = "BACBACCACCBDEDE";
-console.log(solution(str));
-
-// 📚 7. 아나그램
-function solution(str1, str2) {
-  let answer = "YES";
-  let sH = new Map();
-  for (let x of str1) {
-    if (sH.has(x)) sH.set(x, sH.get(x) + 1);
-    else sH.set(x, 1);
-  }
-  for (let x of str2) {
-    if (!sH.has(x) || sH.get(x) == 0) return "NO";
-    sH.set(x, sH.get(x) - 1);
-  }
-  return answer;
-}
-
-let a = "AbaAeCe";
-let b = "baeeACA";
-console.log(solution(a, b));
-
-// 📚 8. 모든 아나그램
-function compareMaps(map1, map2) {
-  if (map1.size !== map2.size) return false;
-  for (let [key, val] of map1) {
-    if (!map2.has(key) || map2.get(key) !== val) return false;
-  }
-  return true;
-}
-function solution(s, t) {
-  let answer = 0;
-  let tH = new Map();
-  let sH = new Map();
-  for (let x of t) {
-    if (tH.has(x)) tH.set(x, tH.get(x) + 1);
-    else tH.set(x, 1);
-  }
-  let len = t.length - 1;
-  for (let i = 0; i < len; i++) {
-    if (sH.has(s[i])) sH.set(s[i], sH.get(s[i]) + 1);
-    else sH.set(s[i], 1);
-  }
-  let lt = 0;
-  for (let rt = len; rt < s.length; rt++) {
-    if (sH.has(s[rt])) sH.set(s[rt], sH.get(s[rt]) + 1);
-    else sH.set(s[rt], 1);
-    if (compareMaps(sH, tH)) answer++;
-    sH.set(s[lt], sH.get(s[lt]) - 1);
-    if (sH.get(s[lt]) === 0) sH.delete(s[lt]);
-    lt++;
-  }
-  return answer;
-}
-
-let a = "bacaAacba";
-let b = "abc";
-console.log(solution(a, b));
+let vote = "BACBACCACCBDEDE";
+console.log(classPresident(vote));
---
+++ 2. Answer/1012 - 자료구조(스택, 큐).js
@@ -0,0 +1,65 @@
+// 📚 1. 올바른 괄호
+function solution(s) {
+  let answer = "YES";
+  stack = [];
+  for (let x of s) {
+    if (x === "(") stack.push(x);
+    else {
+      if (stack.length === 0) return "NO";
+      stack.pop();
+    }
+  }
+  if (stack.length > 0) return "NO";
+  return answer;
+}
+
+let a = "(()(()))(()";
+console.log(solution(a));
+
+// 📚 2. 괄호문자제거
+function solution(s) {
+  let answer;
+  let stack = [];
+  for (let x of s) {
+    if (x === ")") {
+      while (stack.pop() !== "(");
+    } else stack.push(x);
+  }
+  answer = stack.join("");
+  return answer;
+}
+
+let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+console.log(solution(str));
+
+// 📚 3. 크레인 인형뽑기(카카오)
+function solution(board, moves) {
+  let answer = 0;
+  let stack = [];
+  moves.forEach((pos) => {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][pos - 1] !== 0) {
+        let tmp = board[i][pos - 1];
+        board[i][pos - 1] = 0;
+        if (tmp === stack[stack.length - 1]) {
+          stack.pop();
+          answer += 2;
+        } else stack.push(tmp);
+        break;
+      }
+    }
+  });
+
+  return answer;
+}
+
+let a = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+
+let b = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(a, b));
