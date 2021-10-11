0ddb54a97d082e096073aea40167fd92092fe07c
mojaeya <mojaeya@gmail.com>
2021-10-12 00:13:49 +0900

    mojaeya 1012

---
+++ 1. Study/1012 - 자료구조(스택, 큐)/mojaeya/2.괄호문자제거.js
@@ -0,0 +1,15 @@
+function solution(str) {
+  let result;
+  let stack = [];
+  for(let x of str) {
+    if(x === ')') { 
+      while(stack.pop() !== '(');
+    } else stack.push(x)
+    //console.log(stack)
+  }
+  result = stack.join('')
+  return result
+}
+
+let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)"
+console.log(solution(str))
