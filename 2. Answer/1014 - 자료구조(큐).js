cafd71ac8c085ce406e922c35aad3cd49a7e8ba3
ianlee92 <ethian9227@gmail.com>
2021-10-13 21:20:22 +0900

    1014 answer

---
+++ 2. Answer/1014 - 자료구조(큐).js
@@ -0,0 +1,71 @@
+// 📚 4. 후위식 연산
+function solution(s) {
+  let answer;
+  let stack = [];
+  for (let x of s) {
+    if (!isNaN(x)) stack.push(Number(x));
+    else {
+      let rt = stack.pop();
+      let lt = stack.pop();
+      if (x === "+") stack.push(lt + rt);
+      else if (x === "-") stack.push(lt - rt);
+      else if (x === "*") stack.push(lt * rt);
+      else if (x === "/") stack.push(lt / rt);
+    }
+  }
+  answer = stack[0];
+  return answer;
+}
+
+let str = "352+*9-";
+console.log(solution(str));
+
+// 📚 5. 쇠막대기
+function solution(s) {
+  let answer = 0;
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") stack.push("(");
+    else {
+      stack.pop();
+      if (s[i - 1] === "(") answer += stack.length;
+      else answer++;
+      //stack.pop(); 이 위치에 하면 레이저까지 카운팅한다.
+    }
+  }
+  return answer;
+}
+
+let a = "()(((()())(())()))(())";
+console.log(solution(a));
+
+// 📚 6. 공주구하기
+function solution(n, k) {
+  let answer;
+  let queue = Array.from({ length: n }, (v, i) => i + 1);
+  while (queue.length) {
+    for (let i = 1; i < k; i++) queue.push(queue.shift());
+    queue.shift();
+    if (queue.length === 1) answer = queue.shift();
+  }
+  return answer;
+}
+
+console.log(solution(8, 3));
+
+// 📚 7. 교육과정설계
+function solution(need, plan) {
+  let answer = "YES";
+  let queue = need.split("");
+  for (let x of plan) {
+    if (queue.includes(x)) {
+      if (x !== queue.shift()) return "NO";
+    }
+  }
+  if (queue.length > 0) return "NO";
+  return answer;
+}
+
+let a = "CBA";
+let b = "CBDAGE";
+console.log(solution(a, b));
